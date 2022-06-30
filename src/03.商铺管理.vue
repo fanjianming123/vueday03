fb378cf43bd38e854df6e8193908a5cb0bb236d6
newSmile <newSmile007@outlook.com>
2022-06-30 20:00:46 +0800

    帅哥美女走一走完成

---
+++ src/03.商铺管理.vue
@@ -0,0 +1,126 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <!-- 顶部框模块 -->
+      <div class="form-group">
+        <div class="input-group">
+          <h4>品牌管理</h4>
+        </div>
+      </div>
+
+      <!-- 数据表格 -->
+      <table class="table table-bordered table-hover mt-2">
+        <thead>
+          <tr>
+            <th>编号</th>
+            <th>资产名称</th>
+            <th>价格</th>
+            <th>创建时间</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in list" :key="item.id">
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+
+            <!-- 如果价格超过100，就有red这个类 -->
+            <td :class="{ red: item.price > 100 }">{{ item.price }}</td>
+            <td>{{ item.time }}</td>
+            <td><a href="#" @click="del(item.id)">删除</a></td>
+          </tr>
+          <tr style="background-color: #EEE">
+              <td>统计:</td>
+              <td colspan="2">总价钱为: {{total}}</td>
+              <td colspan="2">平均价: {{average}}</td>
+          </tr>
+        </tbody>
+
+        <tfoot>
+          <tr>
+            <td
+              colspan="5"
+              style="text-align: center"
+              v-show="list.length == 0"
+            >
+              暂无数据
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+
+      <!-- 添加资产 -->
+      <form class="form-inline" style="display: flex">
+        <div class="form-group">
+          <div class="input-group">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="资产名称"
+              v-model.trim="name"
+            />
+          </div>
+        </div>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <div class="form-group">
+          <div class="input-group">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="价格"
+              v-model.number="price"
+            />
+          </div>
+        </div>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <!-- 阻止表单提交 -->
+        <button class="btn btn-primary" @click.prevent="addFn">添加资产</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        { id: 100, name: '外套', price: 199, time: new Date('2010-08-12') },
+        { id: 101, name: '裤子', price: 34, time: new Date('2013-09-01') },
+        { id: 102, name: '鞋', price: 25.4, time: new Date('2018-11-22') },
+        { id: 103, name: '头发', price: 19900, time: new Date('2020-12-12') },
+      ],
+      name: '',
+      price: 0,
+    }
+  },
+  methods: {
+    addFn() {
+      this.list.push({
+        id: this?.list[this.list.length - 1]?.id + 1 || 100,
+        name: this.name,
+        price: this.price,
+        time: new Date(),
+      })
+      ;(this.name = ''), (this.price = 0)
+    },
+    del(id) {
+      this.list = this.list.filter((ele) => ele.id !== id)
+    },
+  },
+  computed:{
+    total(){
+      return this.list.reduce((sum, obj) => sum += obj.price, 0)
+    },
+    average(){
+      return (this.total / this.list.length).toFixed(2)
+    }
+  }
+}
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
