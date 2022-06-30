c52ba2be30bee7e52475bef7939283574a8e5058
baibaixy <2942583916@qq.com>
2022-06-30 20:51:13 +0800

    动态绑定style

---
+++ src/6 动态绑定 Class.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p :class="{ active: activeFlag }">我是p标签</p>
+    <button @click="activeFlag = !activeFlag">click</button>
+    <p class="box" :class="{ active: activeFlag }">我也是p</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeFlag: true,
+    };
+  },
+};
+</script>
+
+<style>
+.box {
+  color: blueviolet;
+}
+.active {
+  color: brown;
+}
+</style>
---
+++ src/7 大学案例.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <ul>
+      <li
+        :class="{ active: index === currentIndex }"
+        v-for="(item, index) in navs"
+        :key="index"
+        @click="addFn(index)"
+      >
+        {{ item }}
+      </li>
+      <!-- <li>高中起点</li> -->
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 数据
+      navs: ['大学起点', '高中起点', '初中起点', '小学起点'],
+      currentIndex: 0,
+    };
+  },
+  methods: {
+    addFn(val) {
+      this.currentIndex = val;
+    },
+  },
+};
+</script>
+
+<style>
+ul {
+  list-style: none;
+  border-radius: 10px;
+  width: 400px;
+  overflow: hidden;
+  padding: 0;
+}
+ul li {
+  float: left;
+  width: 100px;
+  height: 40px;
+  background-color: #ccc;
+  color: #fff;
+  text-align: center;
+  line-height: 40px;
+  cursor: pointer;
+}
+li.active {
+  background-color: blue;
+}
+</style>
