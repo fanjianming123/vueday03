826459dcc8bdecca4bd63c0ab591249ab77f5dd0
baibaixy <2942583916@qq.com>
2022-06-30 19:38:35 +0800

    v-show和v-if

---
+++ src/1 v-text和v-html.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ str }}
+    <p v-text="str"></p>
+    <p v-html="str"></p>
+    <p v-html="str">{{ str }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      str: '<span>我是一个span标签</span>',
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/2 v-show和v-if.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 v-show="isShow">我是show</h1>
+    <h2 v-if="isIf">我是if</h2>
+    <p v-if="age >= 18">成年</p>
+    <p v-else>未成年</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: true,
+      isIf: true,
+      age: 18,
+    };
+  },
+};
+</script>
+
+<style></style>
